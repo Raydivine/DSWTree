269d8c5e05d1aa07b85e71ba215937316d9eaf80
Raydivine <raydivine00@gmail.com>
2015-10-09 17:52:37 +0800

    do getNumNodes

---
+++ src/GetNumNodes.c
@@ -0,0 +1,9 @@
+#include "GetNumNodes.h"
+#include "Node.h"
+
+int getNumNodes(Node *vine){
+  int count =0;
+  
+  return count;
+  
+}
---
+++ src/GetNumNodes.h
@@ -0,0 +1,8 @@
+#ifndef GetNumNodes_H
+#define GetNumNodes_H
+#include "Node.h"
+
+int getNumNodes(Node *vine);
+
+
+#endif // GetNumNodes_H
